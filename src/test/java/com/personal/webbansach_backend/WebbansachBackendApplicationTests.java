00f30228280d1f41f7420f4725bbf775b5513e1e
ltabkhn99 <letuananh7111999@gmail.com>
2023-10-07 23:31:14 +0700

    Khoi tao du an

---
+++ src/test/java/com/personal/webbansach_backend/WebbansachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.webbansach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
